4166034cc8cf2ac0df63d5f38777417976f03b4e
Biznaz <aldhelm7@gmail.com>
2021-09-02 15:25:33 -0700

    updates

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,21 +1,19 @@
+
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-
 afterEach(cleanup);
 
-describe('About component', () => {
-  //first test
+describe('About component renders', () => {
   it('renders', () => {
     render(<About />);
   });
-
-  //second test
-  it('matches snapshop DOM node structure', () => {
+  
+  it('matches snapshot DOM node structure', () => {
     const { asFragment } = render(<About />);
+    
     expect(asFragment()).toMatchSnapshot();
-    //render About
-  })
+  });
 })
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,26 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
 
-
-function Gallery (props) {
-  const currentCategory = {
-    name: "commercial",
-    description: "Photos of grocery store, food trucks, and other commercial projects",
-  };
+function Gallery(props) {
+  const { currentCategory } = props;
   return (
     <section>
-      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
       <p>{currentCategory.description}</p>
-      <div className="flex-row">
-        <img
-         src={photo}
-         alt="Commercial Example"
-         className="img-thumbnail mx-1"
-         />
-      </div>
+      <PhotoList category={currentCategory.name} />
     </section>
   );
 }
-
 export default Gallery;
